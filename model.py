dabddc5703b570da24643ad11a32063ca078912d
simony05 <67559452+simony05@users.noreply.github.com>
2024-07-12 18:53:04 -0700

    base model

---
+++ model.py
@@ -0,0 +1,134 @@
+import tensorflow as tf
+import os
+import cv2
+import random
+import warnings
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from termcolor import colored
+import matplotlib.pyplot as plt
+from tensorflow.keras.utils import plot_model
+from keras import optimizers
+from keras import models, layers
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from sklearn.metrics import classification_report, confusion_matrix
+
+# data
+train_dir = 'train'
+validation_dir = 'validation'
+test_dir = 'test'
+
+# count classes
+def num_of_classes(folder_dir, folder_name) :
+    classes = [class_name for class_name in os.listdir(folder_dir)]
+    print(colored(f'number of classes in {folder_name} folder : {len(classes)}', 'blue', attrs=['bold']))
+
+num_of_classes(train_dir, 'train')
+num_of_classes(validation_dir, 'validation')
+num_of_classes(test_dir, 'test')
+
+# samples per class
+classes = [class_name for class_name in os.listdir(train_dir)]
+count = []
+for class_name in classes :
+    count.append(len(os.listdir(os.path.join(train_dir, class_name))))
+
+plt.figure(figsize=(15, 4))
+ax = sns.barplot(x=classes, y=count, color='navy')
+plt.xticks(rotation=285)
+for i in ax.containers:
+    ax.bar_label(i,)
+plt.title('Number of samples per label', fontsize=25, fontweight='bold')
+plt.xlabel('Labels', fontsize=15)
+plt.ylabel('Counts', fontsize=15)
+plt.yticks(np.arange(0, 105, 10))
+# plt.show()
+
+# data to dataframe
+def create_df(folder_path) :
+    all_images = []    
+    for class_name in classes :
+        class_path = os.path.join(folder_path, class_name)
+        all_images.extend([(os.path.join(class_path, file_name), class_name) for file_name in os.listdir(class_path)])
+    df = pd.DataFrame(all_images, columns=['file_path', 'label'])
+    return df
+
+train_df = create_df(train_dir)
+validation_df = create_df(validation_dir)
+test_df = create_df(test_dir)
+
+print(colored(f'Number of samples in train : {len(train_df)}', 'blue', attrs=['bold']))
+print(colored(f'Number of samples in validation : {len(validation_df)}', 'blue', attrs=['bold']))
+print(colored(f'Number of samples test : {len(test_df)}', 'blue', attrs=['bold']))
+
+# example image per class
+df_unique = train_df.copy().drop_duplicates(subset=["label"]).reset_index()
+
+# Display some pictures of the dataset
+fig, axes = plt.subplots(nrows=6, ncols=6, figsize=(8, 7),
+                        subplot_kw={'xticks': [], 'yticks': []})
+
+for i, ax in enumerate(axes.flat):
+    ax.imshow(plt.imread(df_unique.file_path[i]))
+    ax.set_title(df_unique.label[i], fontsize = 12)
+plt.tight_layout(pad=0.5)
+# plt.show()
+
+# data augmentation for more variety + better results
+
+# train generator
+train_datagen = ImageDataGenerator(
+    rescale=1./255,                 # Scaled images in range 0 to 1
+    rotation_range=20,              # Rorate images by factor 20 degree
+    width_shift_range=0.2,          # Shift images horizontally by up to 20% of their width
+    height_shift_range=0.2,         # Shift images vertically by up to 20% of their width
+    zoom_range=0.1,                 # Zoom in and out images by 10%
+    horizontal_flip=True,           # Allow horizontal flipping
+    shear_range=0.1,                # shear images by 10% their size
+    fill_mode='nearest',            # fill unlocated pixels by nearest pixel
+    )
+
+train_generator = train_datagen.flow_from_dataframe(
+    dataframe=train_df,             # Target data
+    x_col='file_path',              # X column
+    y_col='label',                  # y column
+    target_size=(224, 224),         # Resize images  to
+    color_mode='rgb',               # Color mode
+    class_mode='categorical',       # type of model
+    batch_size=32,                  
+    shuffle=True,
+    seed=42,
+)
+
+# validation generator
+
+validation_datagen = ImageDataGenerator(rescale=1./255,)
+
+validation_generator = validation_datagen.flow_from_dataframe(
+    dataframe=validation_df,
+    x_col='file_path',
+    y_col='label',
+    target_size=(224, 224),
+    class_mode='categorical',
+    batch_size=32,
+    seed=42,
+    shuffle=False
+)
+
+# Test generator
+
+test_datagen = ImageDataGenerator(rescale=1./255,)
+
+test_generator = test_datagen.flow_from_dataframe(
+    dataframe=test_df,
+    x_col='file_path',
+    y_col='label',
+    target_size=(224, 224),
+    class_mode='categorical',
+    batch_size=32,
+    seed=42,
+    shuffle=False
+)
